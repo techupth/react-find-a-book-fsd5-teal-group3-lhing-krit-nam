ec2472ba04d4a376a9953d55b4d0ce14624b87ae
Lhing <thorntan.r@gmail.com>
2024-01-02 23:34:17 +0700

    feat: add find a book

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
+import axios from "axios";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  // !!ทำและเรียนรู้ตามเฉลย ต้องกลับมาทบทวน!!
+  // useState ของ Input ที่จะเสิร์ชหาหนังสือ
+  const [inputMessage, setInputMessage] = useState("");
+  // usestate ของ booklist ที่แสดงผลหลังเสิร์ช
+  const [bookList, setBookList] = useState([]);
+  //ฟังก์ชันสำหรับดึงข้อมูลจาก server
+  // function รับ parameter "text" เพื่อมาเสิร์ชหาข้อมูลที่ตรงกับสิ่งที่พิมพ์
+  const getBook = async (text) => {
+    try {
+      const result = await axios.get(
+        `https://www.googleapis.com/books/v1/volumes?q=${text}`
+      );
+      // รับเป็น items เพราะข้อมูลที่เราต้องการ ข้อมูลจาก key "items"
+      setBookList(result.data.items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // สร้าง Request ด้วย Axios ทุกครั้งเมื่อ inputState เปลี่ยน
+    if (inputMessage) {
+      getBook(inputMessage);
+    }
+  }, [inputMessage]);
+  return (
+    <div className="App">
+      {/* start coding here */}
+      <h1>Find a Book</h1>
+      <input
+        type="text"
+        onChange={(event) => {
+          setInputMessage(event.target.value);
+        }}
+        value={inputMessage}
+      />
+      <div className="book-list-state">
+        {bookList.map((item, index) => {
+          return (
+            <div className="book-list" key={index}>
+              <li>{item.volumeInfo.title}</li>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
